304bb67be1a029e8241fc705d82704ad6fece00e
Ondřej Kuzník <ondra@mistotebe.net>
2013-04-01 18:33:08 +0200

    Handle user nonexistence gracefully.

--- imap.c
+++ imap.c
@@ -475,15 +475,20 @@ search_cb(LDAP *ld, LDAPMessage *msg, void *priv)
 {
     struct imap_context *ctx = priv;
     struct bufferevent *server_bev;
-    BerValue **servername;
+    struct evbuffer *out;
+    BerValue **servername = NULL;
 
-    if (!msg) {
-        // user does not exist
+    out = bufferevent_get_output(ctx->client_bev);
+    if (msg) {
+        servername = ldap_get_values_len(ld, msg, "mailhost");
     }
 
-    servername = ldap_get_values_len(ld, msg, "mailhost");
+    // user not provisioned
     if (!servername || !*servername) {
-        // user not provisioned
+        /*FIXME: need the full request to have somethng to send */
+        evbuffer_add_printf(out, "some_tag " AUTH_FAILED_MSG CRLF);
+        bufferevent_enable(ctx->client_bev, EV_READ);
+        return;
     }
 
     server_bev = bufferevent_socket_new(ctx->driver->base, -1, BEV_OPT_CLOSE_ON_FREE);
